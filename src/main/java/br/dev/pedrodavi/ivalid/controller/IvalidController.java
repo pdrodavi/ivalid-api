9d95fab57da3155936ccca419fe262c2cf85f710
Pedro Davi <pedrodaviti@gmail.com>
2022-11-17 02:04:28 -0300

    consulta ie pelo cnpj

--- src/main/java/br/dev/pedrodavi/ivalid/controller/IvalidController.java
+++ src/main/java/br/dev/pedrodavi/ivalid/controller/IvalidController.java
@@ -2,9 +2,23 @@ package br.dev.pedrodavi.ivalid.controller;
 
 import br.dev.pedrodavi.ivalid.service.BrasilApiService;
 import br.dev.pedrodavi.ivalid.service.ViaCepApiService;
+import org.jsoup.Jsoup;
+import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 import static br.dev.pedrodavi.ivalid.utils.ValidaCNPJ.checkCNPJ;
 import static br.dev.pedrodavi.ivalid.utils.ValidaCPF.checkCPF;
@@ -73,5 +87,54 @@ public class IvalidController {
 
         return ResponseEntity.status(400).body("Invalid request!");
     }
+
+    @GetMapping("/search")
+    public ResponseEntity<?> search(@RequestParam String type, @RequestParam String by, @RequestParam String num) {
+
+        if (type.isEmpty() || type.isBlank()) {
+            return ResponseEntity.status(400).body("Type is Required!");
+        }
+
+        if (by.isEmpty() || by.isBlank()) {
+            return ResponseEntity.status(400).body("By is Required!");
+        }
+
+        if (num.isEmpty() || num.isBlank()) {
+            return ResponseEntity.status(400).body("Number is Required!");
+        }
+
+        if (type.equalsIgnoreCase("ie") && by.equalsIgnoreCase("cnpj")) {
+
+            RestTemplate restTemplate = new RestTemplate();
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+            MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+            map.add("tipoDoc","CNPJ");
+            map.add("tpDocumento","2");
+            map.add("nrDocumento",num);
+
+            HttpEntity<MultiValueMap<String, String>> entity = new HttpEntity<>(map, headers);
+
+            ResponseEntity<String> response = restTemplate.postForEntity(
+                    "https://www4.sefaz.pb.gov.br/sintegra/SINf_ConsultaSintegra",
+                    entity, String.class);
+
+            Elements elements = Jsoup.parse(Objects.requireNonNull(response.getBody())).getElementsMatchingOwnText("Inscrição Estadual:");
+            Elements elements2 = Jsoup.parse(Objects.requireNonNull(response.getBody())).getElementsMatchingOwnText("UF:");
+
+            Map<String, String> resp = new HashMap<>();
+            resp.put("IE", elements.next().text());
+            resp.put("UF", elements2.next().text());
+
+            return ResponseEntity.ok(resp);
+        }
+
+        return ResponseEntity.status(400).body("Invalid request!");
+    }
+
+//    public static String html2text(String html) {
+//        return Jsoup.parse(html).text();
+//    }
 
 }
